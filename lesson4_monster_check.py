cd6f424e496d0b90836eb844afb1b62750bf637b
zhenglanyan <2916479410@qq.com>
2026-07-02 16:33:39 +0800

    第4课，学习检测数值

--- lesson4_monster_check.py
+++ lesson4_monster_check.py
@@ -1,17 +1,36 @@
 # 怪物列表
-monsters = ["哥布林", "兽人", "骷髅兵", "食人魔", "巨魔", "狼人", "吸血鬼", "精灵", "恶魔", "巨龙"]
+monster_names = ["哥布林", "兽人", "骷髅兵", "食人魔", "巨魔", "狼人", "吸血鬼", "精灵", "恶魔", "巨龙"]
 
 print("欢迎来到怪物检查程序！")
-print(monsters[0]) # 第一个怪物
-print(monsters[-1]) # 最后一个怪物
-print(len(monsters)) # 怪物总数
+print(monster_names[0]) # 第一个怪物
+print(monster_names[-1]) # 最后一个怪物
+print(len(monster_names)) # 怪物总数
 
-monsters = [
+monsters_data = [
     {"name": "史莱姆", "hp": 50, "attack": 10, "level": 2},
     {"name": "哥布林", "hp": 80, "attack": 15, "level": 3},
     {"name": "巨龙", "hp": 200, "attack": 50, "level": 10},
     {"name": "精灵", "hp": -5, "attack": 20, "level": 4},   # 故意放一个异常血量
-    {"name": "骷髅兵", "hp": 60, "attack": -3, "level": 1}    # 攻击力异常
+    {"name": "骷髅兵", "hp": 60, "attack": -3, "level": 0}    # 攻击力异常
 ]
 
-print
+print("======怪物配置校验======")
+error_details = []
+for monster in monsters_data:
+    errors = []
+    if monster["hp"] <= 0:
+       errors.append(f"血量异常{monster['hp']}")
+    if monster["attack"] <= 0:
+        errors.append(f"攻击力异常{monster['attack']}")
+    if monster["level"] <= 0:
+        errors.append(f"等级异常{monster['level']}")
+
+    if errors:
+        error_details.append({"name": monster["name"], "errors": errors})
+        
+if not error_details:
+    print("所有怪物配置正常。")
+else:
+    print("异常怪物详情：")
+    for detail in error_details:
+        print(f"怪物名称: {detail['name']}, 异常信息: {', '.join(detail['errors'])}")   
